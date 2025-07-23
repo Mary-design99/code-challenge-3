42b8c74335cb52d58a50f8f8b4ccc3128e02685d
Mary-design99 <mary.nyarangi@student.moringaschool.com>
2025-07-23 13:42:35 +0300

    created a movieComponent

---
+++ Development/code/phase-2/Flickster-movies/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState ,useEffect} from 'react'
+import MovieCard from './Components/MovieCard'
+
+function App() {
+  const [movies,setMovies]=useState([]);
+
+  useEffect(()=>{
+    fetch(' http://localhost:3000/moviesAndSeries')
+      .then(res => res.json())// fetched data is converted to json format
+     .then((data)=>{
+      setMovies(data);
+     })
+     }, []);// the empty dependecy array prevents rerendering of app component
+
+  
+
+
+  return (
+    <>
+     <div>
+      <h1>Popular Movies</h1>
+      <div>
+        {movies.length > 0 ? (
+          movies.map(movie => (
+            <MovieCard key={movie.id} movie={movie} image={movie.PosteR} />
+          ))
+        ) : (
+          <p>No movies found.</p>
+        )}
+      </div>
+    </div>
+  
+    
+    </>
+  )
+}
+
+export default App
